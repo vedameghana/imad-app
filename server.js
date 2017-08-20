82866d7193b6c8b8713942636259782afbf8d2d6
vedameghana <31118153+vedameghana@users.noreply.github.com>
2017-08-20 18:55:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function (req,res){
-   res.sendFile(createTemplate(articleOne));
+   res.send(createTemplate(articleOne));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
